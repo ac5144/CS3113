5127fd73105f27ebeb7d4dea5064031fc1b546d5
ac5144 <ac10996@gmail.com>
2015-11-15 23:03:59 -0500

    final project

---
+++ SpaceShooter/NYUCodebase/GameApp.cpp
@@ -0,0 +1,146 @@
+#include "GameApp.h"
+
+GameApp::GameApp() : done(false)
+{
+	Setup();
+}
+
+GameApp::~GameApp() { SDL_Quit(); }
+
+void GameApp::Setup()
+{
+	SDL_Init(SDL_INIT_VIDEO);
+	displayWindow = SDL_CreateWindow("My Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 360, SDL_WINDOW_OPENGL);
+	SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
+	SDL_GL_MakeCurrent(displayWindow, context);
+#ifdef _WINDOWS
+	glewInit();
+#endif
+
+	glViewport(0, 0, 640, 360);
+
+	program = new ShaderProgram(RESOURCE_FOLDER"vertex_textured.glsl", RESOURCE_FOLDER"fragment_textured.glsl");
+	projectionMatrix.setOrthoProjection(-16.0f, 16.0f, -9.0f, 9.0f, -1.0f, 1.0f);
+
+	program->setModelMatrix(modelMatrix);
+	program->setProjectionMatrix(projectionMatrix);
+	program->setViewMatrix(viewMatrix);
+	glUseProgram(program->programID);
+
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	state = MAIN_MENU;
+
+	fontID = LoadTexture("font1.png");
+
+	Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 4096);
+	menuMusic = Mix_LoadMUS("corneria.mp3");
+	Mix_PlayMusic(menuMusic, -1);
+}
+
+void GameApp::ProcessEvents(float elapsed)
+{
+	while (SDL_PollEvent(&event)) {
+		if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE) {
+			done = true;
+		}
+	}
+}
+
+void GameApp::Update(float elapsed)
+{
+	ProcessEvents(elapsed);
+}
+
+void GameApp::Render()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	switch (state)
+	{
+	case MAIN_MENU:
+		RenderMainMenu();
+		break;
+	}
+	SDL_GL_SwapWindow(displayWindow);
+}
+
+bool GameApp::UpdateAndRender()
+{
+	float ticks = (float)SDL_GetTicks() / 1000.0f;
+	float elapsed = ticks - lastFrameTicks;
+	lastFrameTicks = ticks;
+
+	Update(elapsed);
+	Render();
+	return done;
+}
+
+void GameApp::DrawText(int fontTexture, std::string text, float size, float spacing) {
+	float texture_size = 1.0 / 16.0f;
+	std::vector<float> vertexData;
+	std::vector<float> texCoordData;
+	for (int i = 0; i < text.size(); i++) {
+		float texture_x = (float)(((int)text[i]) % 16) / 16.0f;
+		float texture_y = (float)(((int)text[i]) / 16) / 16.0f;
+		vertexData.insert(vertexData.end(), {
+			((size + spacing) * i) + (-0.5f * size), 0.5f * size,
+			((size + spacing) * i) + (-0.5f * size), -0.5f * size,
+			((size + spacing) * i) + (0.5f * size), 0.5f * size,
+			((size + spacing) * i) + (0.5f * size), -0.5f * size,
+			((size + spacing) * i) + (0.5f * size), 0.5f * size,
+			((size + spacing) * i) + (-0.5f * size), -0.5f * size,
+		});
+		texCoordData.insert(texCoordData.end(), {
+			texture_x, texture_y,
+			texture_x, texture_y + texture_size,
+			texture_x + texture_size, texture_y,
+			texture_x + texture_size, texture_y + texture_size,
+			texture_x + texture_size, texture_y,
+			texture_x, texture_y + texture_size,
+		});
+	}
+	glUseProgram(program->programID);
+	glVertexAttribPointer(program->positionAttribute, 2, GL_FLOAT, false, 0, vertexData.data());
+	glEnableVertexAttribArray(program->positionAttribute);
+	glVertexAttribPointer(program->texCoordAttribute, 2, GL_FLOAT, false, 0, texCoordData.data());
+	glEnableVertexAttribArray(program->texCoordAttribute);
+	glBindTexture(GL_TEXTURE_2D, fontTexture);
+	glDrawArrays(GL_TRIANGLES, 0, text.size() * 6);
+	glDisableVertexAttribArray(program->positionAttribute);
+	glDisableVertexAttribArray(program->texCoordAttribute);
+}
+
+GLuint GameApp::LoadTexture(const char* image_path)
+{
+	SDL_Surface* surface = IMG_Load(image_path);
+
+	GLuint textureID;
+	glGenTextures(1, &textureID);
+	glBindTexture(GL_TEXTURE_2D, textureID);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, surface->w, surface->h, 0, GL_RGBA, GL_UNSIGNED_BYTE, surface->pixels);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	SDL_FreeSurface(surface);
+
+	return textureID;
+}
+
+void GameApp::RenderMainMenu()
+{
+	modelMatrix.identity();
+	modelMatrix.Translate(-12.0, 3.0, 0.0);
+	program->setModelMatrix(modelMatrix);
+	DrawText(fontID, "Space Shooter", 3.5, -1.5);
+
+	modelMatrix.identity();
+	modelMatrix.Translate(-7.0, -1.0, 0.0);
+	program->setModelMatrix(modelMatrix);
+	DrawText(fontID, "Press Enter", 1.5, -0.0);
+}
---
+++ SpaceShooter/NYUCodebase/GameApp.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#ifdef _WINDOWS
+#include <GL/glew.h>
+#endif
+#include <SDL.h>
+#include <SDL_opengl.h>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
+#include "Matrix.h"
+#include "ShaderProgram.h"
+#include <vector>
+#include <string>
+
+#ifdef _WINDOWS
+#define RESOURCE_FOLDER ""
+#else
+#define RESOURCE_FOLDER "NYUCodebase.app/Contents/Resources/"
+#endif
+enum GameState {MAIN_MENU};
+
+class GameApp
+{
+public:
+	GameApp();
+	~GameApp();
+
+	void Setup();
+	void ProcessEvents(float elapsed);
+	void Update(float elapsed);
+	void Render();
+	bool UpdateAndRender();
+	void DrawText(int fontTexture, std::string text, float size, float spacing);
+	GLuint LoadTexture(const char* image_path);
+	void RenderMainMenu();
+
+private:
+	SDL_Window* displayWindow;
+	Matrix modelMatrix;
+	Matrix projectionMatrix;
+	Matrix viewMatrix;
+	ShaderProgram *program;
+	SDL_Event event;
+	bool done;
+	int fontID;
+	float lastFrameTicks;
+	GameState state;
+	Mix_Music *menuMusic;
+};
+
